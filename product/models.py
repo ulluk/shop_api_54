a2381c85ec31fecf4c93efdcd8cae6f912366ab6
Bekbolsunn <karataevbekbolsun@gmail.com>
2025-08-26 19:05:21 +0600

    initialization of the project

---
+++ product/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+STARS =(
+    (i,"⭐" * i) for i in range(1,6)
+)
+
+class Review(models.Model):
+    text = models.TextField(null=True,blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,related_name='reviews')
+    stars = models.IntegerField(choices=STARS, default=5)
+    def __str__(self):
+        return f'Отзыв на {self.product.title}'
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class ConfirmationCode(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='confirmation_code')
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Код подтверждения для {self.user.username}"
